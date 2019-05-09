535da7dc3127127f4720ce1908ba24a0ab0451bb
Hristiyan Dodov <h.dodov@gmail.com>
2019-05-09 13:46:14 +0300

    Add sick field previews for structures

--- app/index.js
+++ app/index.js
@@ -1,7 +1,11 @@
 import App from './App.vue'
+import Preview from './Preview.vue'
 
 panel.plugin('medienbaecker/link', {
   fields: {
     link: App
+  },
+  components: {
+    'k-link-field-preview': Preview
   }
 })
