2d35b01db02af7ef8ec83aa6b13e2b454f8a5a7a
Hristiyan Dodov <h.dodov@gmail.com>
2019-08-01 10:54:03 +0300

    Apply PSR-4, add autoloading

--- index.php
+++ index.php
@@ -1,112 +1,107 @@
 <?php
 
-namespace LinkField;
+namespace Oblik\Kirby\LinkField;
 
 use Yaml;
 use Kirby;
-use LinkField\Link;
-
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Link.php';
 
 Kirby::plugin('oblik/link-field', [
-  'fields' => [
-    'link' => [
-      'mixins' => ['pagepicker', 'filepicker'],
-      'props' => [
-        'value' => function ($data = []) {
-          if (is_string($data)) {
-            $data = Yaml::decode($data);
-          }
+    'fields' => [
+        'link' => [
+            'mixins' => ['pagepicker', 'filepicker'],
+            'props' => [
+                'value' => function ($data = []) {
+                    if (is_string($data)) {
+                        $data = Yaml::decode($data);
+                    }
 
-          $type = $data['type'] ?? null;
-          $value = $data['value'] ?? null;
+                    $type = $data['type'] ?? null;
+                    $value = $data['value'] ?? null;
 
-          if (!$type || !$value) {
-            return null;
-          }
+                    if (!$type || !$value) {
+                        return null;
+                    }
 
-          if (is_string($value)) {
-            if ($type === 'page' && $page = kirby()->page($value)) {
-              $data['value'] = [$this->pageResponse($page)];
-            } else if ($type === 'file' && $file = kirby()->file($value, $this->model())) {
-              $data['value'] = [$this->fileResponse($file)];
+                    if (is_string($value)) {
+                        if ($type === 'page' && $page = kirby()->page($value)) {
+                            $data['value'] = [$this->pageResponse($page)];
+                        } else if ($type === 'file' && $file = kirby()->file($value, $this->model())) {
+                            $data['value'] = [$this->fileResponse($file)];
+                        }
+                    }
+
+                    return $data;
+                },
+                'options' => function ($value = ['url', 'page', 'file', 'email', 'tel']) {
+                    return $value;
+                },
+                'settings' => function ($value = []) {
+                    return $value;
+                }
+            ],
+            'methods' => [
+                'pageResponse' => function ($page) {
+                    return $page->panelPickerData();
+                },
+                'fileResponse' => function ($file) {
+                    return $file->panelPickerData([
+                        'text' => '{{ file.id }}'
+                    ]);
+                }
+            ],
+            'api' => function () {
+                return [
+                    [
+                        'pattern' => '/link-files',
+                        'method' => 'GET',
+                        'action' => function () {
+                            return $this->field()->filepicker([
+                                'query' => 'site.files.add(site.index.files)',
+                                'text' => '{{ file.id }}'
+                            ]);
+                        }
+                    ],
+                    [
+                        'pattern' => '/link-pages',
+                        'method' => 'GET',
+                        'action' => function () {
+                            return $this->field()->pagepicker([
+                                'parent' => $this->requestQuery('parent')
+                            ]);
+                        }
+                    ]
+                ];
+            },
+            'save' => function ($data) {
+                $type = $data['type'] ?? null;
+                $value = $data['value'] ?? null;
+
+                if (!$type || !$value) {
+                    return null;
+                }
+
+                // Store just the id of a file or a page.
+                if ($type === 'page' || $type === 'file') {
+                    $data['value'] = $value[0]['id'] ?? null;
+                }
+
+                return $data;
             }
-          }
+        ]
+    ],
+    'translations' => [
+        'en' => require_once __DIR__ . '/languages/en.php',
+        'de' => require_once __DIR__ . '/languages/de.php'
+    ],
+    'fieldMethods' => [
+        'toLink' => function ($field) {
+            $data = $field->yaml();
 
-          return $data;
-        },
-        'options' => function ($value = ['url', 'page', 'file', 'email', 'tel']) {
-          return $value;
-        },
-        'settings' => function ($value = []) {
-          return $value;
-        }
-      ],
-      'methods' => [
-        'pageResponse' => function ($page) {
-          return $page->panelPickerData();
-        },
-        'fileResponse' => function ($file) {
-          return $file->panelPickerData([
-            'text' => '{{ file.id }}'
-          ]);
-        }
-      ],
-      'api' => function () {
-        // Routes sould not be `files` or `pages` because they could conflict
-        // with native Kirby routes.
-        return [
-          [
-            'pattern' => '/link-files',
-            'method' => 'GET',
-            'action' => function () {
-              return $this->field()->filepicker([
-                'query' => 'site.files.add(site.index.files)',
-                'text' => '{{ file.id }}'
-              ]);
+            if (!empty($data['type']) && !empty($data['value'])) {
+                return new Link($field, $data);
+            } else {
+                return null;
             }
-          ],
-          [
-            'pattern' => '/link-pages',
-            'method' => 'GET',
-            'action' => function () {
-              return $this->field()->pagepicker([
-                'parent' => $this->requestQuery('parent')
-              ]);
-            }
-          ]
-        ];
-      },
-      'save' => function ($data) {
-        $type = $data['type'] ?? null;
-        $value = $data['value'] ?? null;
-
-        if (!$type || !$value) {
-          return null;
         }
-
-        // Store just the id of a file or a page.
-        if ($type === 'page' || $type === 'file') {
-          $data['value'] = $value[0]['id'] ?? null;
-        }
-
-        return $data;
-      }
     ]
-  ],
-  'translations' => [
-    'en' => require_once __DIR__ . '/languages/en.php',
-    'de' => require_once __DIR__ . '/languages/de.php'
-  ],
-  'fieldMethods' => [
-    'toLink' => function ($field) {
-      $data = $field->yaml();
-
-      if (!empty($data['type']) && !empty($data['value'])) {
-        return new Link($field, $data);
-      } else {
-        return null;
-      }
-    }
-  ]
 ]);
--- src/Link.php
+++ src/Link.php
@@ -1,126 +1,138 @@
 <?php
-namespace LinkField;
+
+namespace Oblik\Kirby\LinkField;
 
 use Kirby\Http\Url;
 use Kirby\Toolkit\Str;
 use Kirby\Toolkit\Html;
 
-class Link {
-  function __construct ($field, $data) {
-    $this->field = $field;
-    $this->data = $data;
+class Link
+{
+    function __construct($field, $data)
+    {
+        $this->field = $field;
+        $this->data = $data;
 
-    $this->type = $this->data['type'];
-    $this->value = $this->data['value'];
-    $this->popup = $this->data['popup'] ?? false;
-    $this->hash = $this->data['hash'] ?? null;
-    $this->text = $this->data['text'] ?? null;
+        $this->type = $this->data['type'];
+        $this->value = $this->data['value'];
+        $this->popup = $this->data['popup'] ?? false;
+        $this->hash = $this->data['hash'] ?? null;
+        $this->text = $this->data['text'] ?? null;
 
-    $this->page = null;
-    $this->file = null;
+        $this->page = null;
+        $this->file = null;
 
-    if ($this->value) {
-      if ($this->type === 'page') {
-        $this->page = kirby()->page($this->value);
-      }
+        if ($this->value) {
+            if ($this->type === 'page') {
+                $this->page = kirby()->page($this->value);
+            }
 
-      if ($this->type === 'file') {
-        $this->file = kirby()->file($this->value, $this->field->model());
-      }
+            if ($this->type === 'file') {
+                $this->file = kirby()->file($this->value, $this->field->model());
+            }
+        }
+
+        $this->parts = Url::toObject($this->url() ?? '');
     }
 
-    $this->parts = Url::toObject($this->url() ?? '');
-  }
-
-  function __call ($name, $arguments = []) {
-    return $this->parts->$name();
-  }
-
-  function __toString () {
-    return $this->href();
-  }
-
-  public function text () {
-    return $this->text ?? '';
-  }
-
-  public function title () {
-    $text = $this->text;
-
-    if (!empty($text)) {
-      return $text;
+    function __call($name, $arguments = [])
+    {
+        return $this->parts->$name();
     }
 
-    if ($this->page) {
-      $text = $this->page->title()->value();
+    function __toString()
+    {
+        return $this->href();
     }
 
-    if ($this->file) {
-      $text = $this->file->title()->value();
-
-      if (!$text) {
-        $text = $this->file->filename();
-      }
+    public function text()
+    {
+        return $this->text ?? '';
     }
 
-    if (empty($text)) {
-      $text = $this->value;
+    public function title()
+    {
+        $text = $this->text;
+
+        if (!empty($text)) {
+            return $text;
+        }
+
+        if ($this->page) {
+            $text = $this->page->title()->value();
+        }
+
+        if ($this->file) {
+            $text = $this->file->title()->value();
+
+            if (!$text) {
+                $text = $this->file->filename();
+            }
+        }
+
+        if (empty($text)) {
+            $text = $this->value;
+        }
+
+        return $text;
     }
 
-    return $text;
-  }
+    public function url()
+    {
+        $value = null;
 
-  public function url () {
-    $value = null;
+        if ($this->type === 'url') {
+            $value = $this->value;
+        } else if ($this->type === 'page' && $this->page) {
+            $value = $this->page->url();
+        } else if ($this->type === 'file' && $this->file) {
+            $value = $this->file->url();
+        }
 
-    if ($this->type === 'url') {
-      $value = $this->value;
-    } else if ($this->type === 'page' && $this->page) {
-      $value = $this->page->url();
-    } else if ($this->type === 'file' && $this->file) {
-      $value = $this->file->url();
+        if (is_string($value) && $this->hash) {
+            $value .= '#' . $this->hash;
+        }
+
+        return $value;
     }
 
-    if (is_string($value) && $this->hash) {
-      $value .= '#' . $this->hash;
+    public function href()
+    {
+        if ($this->type === 'tel') {
+            return 'tel:' . Str::replace($this->value, [' ', '/', '-', '.'], '');
+        } else if ($this->type === 'email') {
+            return 'mailto:' . $this->value;
+        } else {
+            return $this->url();
+        }
     }
 
-    return $value;
-  }
+    public function attributes($attr = [])
+    {
+        $attributes = [
+            'href' => $this->href()
+        ];
 
-  public function href () {
-    if ($this->type === 'tel') {
-      return 'tel:' . Str::replace($this->value, [' ', '/', '-', '.'], '');
-    } else if ($this->type === 'email') {
-      return 'mailto:' . $this->value;
-    } else {
-      return $this->url();
-    }
-  }
+        if ($this->popup === true) {
+            $attributes['target'] = '_blank';
+        }
 
-  public function attributes ($attr = []) {
-    $attributes = [
-      'href' => $this->href()
-    ];
-
-    if ($this->popup === true) {
-      $attributes['target'] = '_blank';
+        return array_merge($attributes, $attr);
     }
 
-    return array_merge($attributes, $attr);
-  }
-
-  public function attr ($attr = []) {
-    return Html::attr($this->attributes($attr));
-  }
-
-  public function tag ($attr = []) {
-    $href = $this->href();
-
-    if (strpos($href, 'http') === 0) { // https://github.com/getkirby/kirby/issues/1734
-      return Html::a($href, $this->title(), $this->attributes($attr)); // https://github.com/getkirby/kirby/issues/1733
-    } else {
-      return Html::tag('a', $this->title(), $this->attributes($attr));
+    public function attr($attr = [])
+    {
+        return Html::attr($this->attributes($attr));
     }
-  }
+
+    public function tag($attr = [])
+    {
+        $href = $this->href();
+
+        if (strpos($href, 'http') === 0) { // https://github.com/getkirby/kirby/issues/1734
+            return Html::a($href, $this->title(), $this->attributes($attr)); // https://github.com/getkirby/kirby/issues/1733
+        } else {
+            return Html::tag('a', $this->title(), $this->attributes($attr));
+        }
+    }
 }
