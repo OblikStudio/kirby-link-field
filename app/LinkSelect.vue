e479c5688ea147c06e4a0ed0b1df0d83cba90e07
Hristiyan Dodov <h.dodov@gmail.com>
2022-06-20 11:06:41 +0300

    Switch to kirbyup

--- app/LinkSelect.vue
+++ app/LinkSelect.vue
@@ -112,8 +112,8 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-/deep/ .k-field-header {
-  display: none; // hides the Select buttons
+<style scoped>
+::v-deep .k-field-header {
+  display: none; /* hides the Select buttons */
 }
 </style>
