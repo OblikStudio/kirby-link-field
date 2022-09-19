c205e8830edb57dbb3a64fae6811a75382a39059
Samuel Rivers-Moore <samuel.riversmoore@gmail.com>
2022-09-19 17:27:18 +0200

    Add french translation

---
+++ languages/fr.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+  'url' => 'URL',
+  'page' => 'Page',
+  'file' => 'Fichier',
+  'email' => 'Email',
+  'tel' => 'Téléphone',
+  'label.text' => 'Texte',
+  'label.popup' => 'Popup',
+  'label.fragment' => 'Ancre',
+  'label.settings' => 'Réglages',
+  'label.close' => 'Fermer',
+  'placeholder.elementid' => 'Identifiant de l\'élément',
+  'error.type' => 'Type de champ invalide: {type}'
+];
