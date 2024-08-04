d1bdda49afe7bb983add89a99c7cd53776f074bf
Hristiyan Dodov <h.dodov@gmail.com>
2024-08-04 10:58:35 +0300

    Fix text preview content

--- site/templates/default.php
+++ site/templates/default.php
@@ -3,14 +3,14 @@
 	<li>
 		<ul>
 			<?php foreach (site()->structure()->toStructure() as $entry) : ?>
-				<li><?= $entry->link()->toLinkObject()->tag() ?></li>
+				<li><?= $entry->legacyLink()->toLinkObject()->tag() ?></li>
 			<?php endforeach ?>
 		</ul>
 	</li>
 	<li>
 		<ul>
 			<?php foreach (site()->blocks()->toBlocks() as $block) : ?>
-				<li><?= $block->link()->toLinkObject()->tag() ?></li>
+				<li><?= $block->legacyLink()->toLinkObject()->tag() ?></li>
 			<?php endforeach ?>
 		</ul>
 	</li>
